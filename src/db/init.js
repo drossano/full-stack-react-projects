b8f9da7efbad001a1872e86b98b746d03961bca3
drossano <deanrossano+dev@gmail.com>
2025-09-14 18:08:34 -0400

    build: set up express

--- src/db/init.js
+++ src/db/init.js
@@ -1,9 +1,8 @@
 import mongoose from "mongoose";
-
 export function initDatabase() {
   const DATABASE_URL = process.env.DATABASE_URL;
   mongoose.connection.on("open", () => {
-    console.info("successfully connected to db", DATABASE_URL);
+    console.info("successfully connected to database: ", DATABASE_URL);
   });
   const connection = mongoose.connect(DATABASE_URL);
   return connection;
