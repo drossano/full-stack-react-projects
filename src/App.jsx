61c0eef19c89a7091e76d33eb9d78d3b03254aab
drossano <deanrossano+dev@gmail.com>
2025-09-15 18:40:32 -0400

    feat: implement all UI elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import { PostList } from "./components/PostList.jsx";
 import { CreatePost } from "./components/CreatePost.jsx";
+import { PostFilter } from "./components/PostFilter.jsx";
+import { PostSorting } from "./components/PostSorting.jsx";
+import "./App.css";
+
+const posts = [
+  {
+    title: "Full-Stack React Projects",
+    contents: "Let's become full-stack developers!",
+    author: "Daniel Bugl",
+  },
+  {
+    title: "Hello React!",
+  },
+];
 
 export function App() {
-  return <CreatePost />;
+  return (
+    <div style={{ padding: 8 }}>
+      <CreatePost />
+      <br />
+      <hr />
+      Filter by:
+      <PostFilter field="author" />
+      <br />
+      <PostSorting fields={["createdAt", "updatedAt"]} />
+      <hr />
+      <PostList posts={posts} />
+    </div>
+  );
 }
