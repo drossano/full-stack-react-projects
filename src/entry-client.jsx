25e0a9af222596aeb0b57ea4731ac36e2418e8a3
drossano <deanrossano+dev@gmail.com>
2025-09-26 12:56:00 -0400

    feat: redefine routerin client entry point

--- src/entry-client.jsx
+++ src/entry-client.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { App } from "./App.jsx";
+import { routes } from "./routes.jsx";
+
+const router = createBrowserRouter(routes);
 
 ReactDOM.hydrateRoot(
   document.getElementById("root"),
   <React.StrictMode>
-    <App />
+    <App>
+      <RouterProvider router={router} />
+    </App>
   </React.StrictMode>,
 );
