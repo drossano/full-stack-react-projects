027978fd2a3e1ba59bfa4d460d49c4baf5233bb4
drossano <deanrossano+dev@gmail.com>
2025-10-06 12:47:47 -0400

    feat: add aggregation api functions

--- src/api/events.js
+++ src/api/events.js
@@ -6,3 +6,18 @@ export const postTrackEvent = (event) =>
     },
     body: JSON.stringify(event),
   }).then((res) => res.json());
+
+export const getTotalViews = (postId) =>
+  fetch(`${import.meta.env.VITE_BACKEND_URL}/evens/totalViews/${postId}`).then(
+    (res) => res.json(),
+  );
+
+export const getDailyViews = (postId) =>
+  fetch(`${import.meta.env.VITE_BACKEND_URL}/evens/dailyViews/${postId}`).then(
+    (res) => res.json(),
+  );
+
+export const getDailyDurations = (postId) =>
+  fetch(
+    `${import.meta.env.VITE_BACKEND_URL}/evens/dailyDurations/${postId}`,
+  ).then((res) => res.json());
