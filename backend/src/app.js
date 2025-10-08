e23248d8163feed0ab78d266019c9d9b2d056743
drossano <deanrossano+dev@gmail.com>
2025-10-08 12:51:22 -0400

    feat: implement test graphql field

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,9 @@ import express from 'express'
 import { postsRoutes } from './routes/posts.js'
 import { userRoutes } from './routes/users.js'
 import { eventRoutes } from './routes/events.js'
+import { ApolloServer } from '@apollo/server'
+import { expressMiddleware } from '@apollo/server/express4'
+import { typeDefs, resolvers } from './graphql/index.js'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 
@@ -11,6 +14,15 @@ app.use(bodyParser.json())
 postsRoutes(app)
 userRoutes(app)
 eventRoutes(app)
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+apolloServer
+  .start()
+  .then(() => app.use('/graphql', expressMiddleware(apolloServer)))
 
 app.get('/', (req, res) => {
   res.send('Hello from Express!')
