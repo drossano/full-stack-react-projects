5f5f7f79983eaf0dc8070dcf401c36b4a1b2f598
drossano <deanrossano+dev@gmail.com>
2025-10-08 13:28:37 -0400

    feat: implement fields to query posts

--- backend/src/graphql/query.js
+++ backend/src/graphql/query.js
@@ -1,12 +1,35 @@
+import {
+  getPostById,
+  listAllPosts,
+  listPostsByAuthor,
+  listPostsByTag,
+} from '../services/posts.js'
+
 export const querySchema = `#graphql
 type Query {
   test: String
+  posts: [Post!]!
+  postsByAuthor(username: String!): [Post!]!
+  postsByTag(tag: String!): [Post!]!
+  postsById(id: ID!): Post
 }`
 
 export const queryResolver = {
   Query: {
     test: () => {
       return 'Hello world from GraphQL!'
+    },
+    posts: async () => {
+      return await listAllPosts()
+    },
+    postsByAuthor: async (parent, { username }) => {
+      return await listPostsByAuthor(username)
+    },
+    postsByTag: async (parent, { tag }) => {
+      return await listPostsByTag(tag)
+    },
+    postsById: async (parent, { id }) => {
+      return await getPostById(id)
     },
   },
 }
