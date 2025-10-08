665efd70a64b566d2675f70411e4d8379d0fe953
drossano <deanrossano+dev@gmail.com>
2025-10-08 19:21:20 -0400

    feat: add signing up, logging in and posting with graphql

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,7 @@ import { eventRoutes } from './routes/events.js'
 import { ApolloServer } from '@apollo/server'
 import { expressMiddleware } from '@apollo/server/express4'
 import { typeDefs, resolvers } from './graphql/index.js'
+import { optionalAuth } from './middleware/jwt.js'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 
@@ -20,9 +21,17 @@ const apolloServer = new ApolloServer({
   resolvers,
 })
 
-apolloServer
-  .start()
-  .then(() => app.use('/graphql', expressMiddleware(apolloServer)))
+apolloServer.start().then(() =>
+  app.use(
+    '/graphql',
+    optionalAuth,
+    expressMiddleware(apolloServer, {
+      context: async ({ req }) => {
+        return { auth: req.auth }
+      },
+    }),
+  ),
+)
 
 app.get('/', (req, res) => {
   res.send('Hello from Express!')
