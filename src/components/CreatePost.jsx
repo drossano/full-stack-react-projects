bc60bd86d30bf0322e481d9ad13ec0185cc7d96d
drossano <deanrossano+dev@gmail.com>
2025-10-10 20:21:48 -0400

    feat: migrate posting to graphql

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,20 +1,26 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation } from "@apollo/client/react/index.js";
+import { Link } from "react-router-dom";
+import slug from "slug";
 import { useState } from "react";
-import { createPost } from "../api/posts.js";
+import {
+  CREATE_POST,
+  GET_POSTS,
+  GET_POSTS_BY_AUTHOR,
+} from "../api/graphql/posts.js";
 import { useAuth } from "../contexts/AuthContext.jsx";
 
 export function CreatePost() {
   const [token] = useAuth();
   const [title, setTitle] = useState("");
   const [contents, setContents] = useState("");
-  const queryClient = useQueryClient();
-  const createPostMutation = useMutation({
-    mutationFn: () => createPost(token, { title, contents }),
-    onSuccess: () => queryClient.invalidateQueries(["posts"]),
+  const [createPost, { loading, data }] = useMutation(CREATE_POST, {
+    variables: { title, contents },
+    context: { headers: { Authorization: `Bearer ${token}` } },
+    refetchQueries: [GET_POSTS, GET_POSTS_BY_AUTHOR],
   });
   const handleSubmit = (e) => {
     e.preventDefault();
-    createPostMutation.mutate();
+    createPost();
   };
   if (!token) return <div>Please log in to create new posts.</div>;
   return (
@@ -38,9 +44,21 @@ export function CreatePost() {
       <br />
       <input
         type="submit"
-        value={createPostMutation.isPending ? "Creating..." : "Create"}
-        disabled={!title || createPostMutation.isPending}
+        value={loading ? "Creating..." : "Create"}
+        disabled={!title || loading}
       />
+      {data?.createPost ? (
+        <>
+          <br />
+          Post{" "}
+          <Link
+            to={`/posts/${data.createPost.id}/${slug(data.createPost.title)}`}
+          >
+            {data.createPost.title}
+          </Link>{" "}
+          created successfully!
+        </>
+      ) : null}
     </form>
   );
 }
